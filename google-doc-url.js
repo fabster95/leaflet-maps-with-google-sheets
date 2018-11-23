a961c809544d8eb4d59dc45aaea50988f7ccd958
fabster95 <34654584+fabster95@users.noreply.github.com>
2018-11-23 16:32:44 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1X9OsiqOD0x0LmgriTByBj4_tfPhz0F26LcnlT81bzZ0/edit#gid=0';
+var googleDocURL ='https://docs.google.com/spreadsheets/d/1X9OsiqOD0x0LmgriTByBj4_tfPhz0F26LcnlT81bzZ0/edit#gid=0';
